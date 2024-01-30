0c61e4b839b7acc235c63c554db0e8c50941213c
CharlyLuna <cluna10@ucol.mx>
2024-01-30 17:55:57 -0600

    Create server actions page

--- admin-todos/src/app/dashboard/server-actions/page.tsx
+++ admin-todos/src/app/dashboard/server-actions/page.tsx
@@ -2,15 +2,16 @@ import prisma from "@/lib/prisma"
 import { NewTodo, TodosGrid } from "@/todos"
 
 export const metadata = {
-  title: "List of Todos",
-  description: "List of Todos",
+  title: "Server actions",
+  description: "Server actions to retrieve todos",
 }
 
-export default async function RestTodosPage() {
+export default async function ServerActionsPage() {
   const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } })
 
   return (
     <div>
+      <h1 className='mb-4 text-2xl font-bold'>Server Actions</h1>
       <div className='flex justify-center items-center mb-4'>
         <NewTodo />
       </div>
