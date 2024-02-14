207dc111e27ef3598302039ed2fc2fc03a9483f6
CharlyLuna <cluna10@ucol.mx>
2024-02-14 17:05:25 -0600

    Show total price of the cart

--- admin-todos/src/app/dashboard/cart/page.tsx
+++ admin-todos/src/app/dashboard/cart/page.tsx
@@ -1,19 +1,20 @@
 import { cookies } from "next/headers"
 import { ItemCard, type CartItem } from "@/shopping-cart"
 import { Product, products } from "@/products"
+import { WidgetItem } from "@/components"
 
 export const metadata = {
   title: "Products on the cart | Cart Page",
   description: "Products on your cart",
 }
 
-interface ProductInCart {
+interface ProductsInCart {
   product: Product
   quantity: number
 }
 
-const getProductsInCart = (cart: CartItem): ProductInCart[] => {
-  const productsInCart: ProductInCart[] = []
+const getProductsInCart = (cart: CartItem): ProductsInCart[] => {
+  const productsInCart: ProductsInCart[] = []
   for (const productId of Object.keys(cart)) {
     const product = products.find((product) => product.id === productId)
     if (product) {
@@ -23,10 +24,18 @@ const getProductsInCart = (cart: CartItem): ProductInCart[] => {
   return productsInCart
 }
 
+const getTotalPrice = (products: ProductsInCart[]): number => {
+  let sum = products.reduce((accumulator, current) => {
+    return current.product.price * current.quantity + accumulator
+  }, 0)
+  return sum
+}
+
 export default function CartPage() {
   const cookiesStore = cookies()
   const cart = JSON.parse(cookiesStore.get("cart")?.value ?? "{}") as CartItem
   const products = getProductsInCart(cart)
+  const totalPrice = getTotalPrice(products)
 
   return (
     <div>
@@ -36,6 +45,18 @@ export default function CartPage() {
           {products.map(({ product, quantity }) => (
             <ItemCard key={product.id} product={product} quantity={quantity} />
           ))}
+        </div>
+        <div className='flex flex-col w-full sm:w-4/12'>
+          <WidgetItem title='Total price'>
+            <div className='mt-2 flex justify-center gap-4'>
+              <h3 className='text-2xl font-bold text-gray-700'>
+                ${(totalPrice * 1.15).toFixed(2)}
+              </h3>
+            </div>
+            <span className='font-bold text-center text-gray-600'>
+              Impuestos 15%: ${(totalPrice * 0.15).toFixed(2)}
+            </span>
+          </WidgetItem>
         </div>
       </div>
     </div>
--- admin-todos/src/app/dashboard/page.tsx
+++ admin-todos/src/app/dashboard/page.tsx
@@ -3,11 +3,10 @@ import { WidgetItem } from "@/components"
 export default function DashboardPage() {
   return (
     <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-      <WidgetItem
+      {/* <WidgetItem
         title='Global Activities'
         subtitle='Compared to last week $13,988'
-        value='$23,988'
-      />
+      /> */}
     </div>
   )
 }
