5e04d1308be44327ed60623083738a05f8cf736d
CharlyLuna <cluna10@ucol.mx>
2024-02-16 17:30:33 -0600

    Add signIn using password and email

--- admin-todos/src/app/api/auth/[...nextauth]/route.ts
+++ admin-todos/src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import { Adapter } from "next-auth/adapters"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { signInEmailPassword } from "@/auth/actions/auth-actions"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -15,6 +17,33 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(
+          credentials!.email,
+          credentials!.password
+        )
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
   session: {
---
+++ admin-todos/src/auth/actions/auth-actions.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma"
+import bcrypt from "bcryptjs"
+
+export const signInEmailPassword = async (email: string, password: string) => {
+  if (email.length < 6 || password.length < 6) {
+    return null
+  }
+  const user = await prisma.user.findUnique({ where: { email } })
+  if (!user) {
+    const dbuser = await createUser(email, password)
+    return dbuser
+  }
+  if (!bcrypt.compareSync(password, user.password ?? "")) {
+    return null
+  }
+  return user
+}
+
+const createUser = async (email: string, password: string) => {
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: bcrypt.hashSync(password),
+      name: email.split("@")[0],
+    },
+  })
+  return user
+}
