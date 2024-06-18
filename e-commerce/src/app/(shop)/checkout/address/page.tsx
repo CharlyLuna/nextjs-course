282c153d2f32d924bacdb7633a0cfe286cea155f
CharlyLuna <cluna10@ucol.mx>
2024-06-17 21:28:43 -0600

    Fix middleware matcher and add address form component

--- e-commerce/src/app/(shop)/checkout/address/page.tsx
+++ e-commerce/src/app/(shop)/checkout/address/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { Title } from "@/components"
+import { AddressForm } from "./ui/AddressForm"
 
 export default function AddressPage() {
   return (
@@ -7,59 +8,7 @@ export default function AddressPage() {
       <div className='w-full  xl:w-[1000px] flex flex-col justify-center text-left'>
         <Title title='Address' subtitle='Delivery address' />
 
-        <div className='grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2 bg-white rounded-xl shadow-xl p-7'>
-          <div className='flex flex-col mb-2'>
-            <span>Name</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>Last name</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>Address</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>Second address (opcional)</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>CP</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>City</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>Country</span>
-            <select className='p-2 border rounded-md bg-gray-200'>
-              <option value=''>[ Seleccione ]</option>
-              <option value='CRI'>Costa Rica</option>
-            </select>
-          </div>
-
-          <div className='flex flex-col mb-2'>
-            <span>Phone</span>
-            <input type='text' className='p-2 border rounded-md bg-gray-200' />
-          </div>
-
-          <div className='flex flex-col mb-2 sm:mt-10'>
-            <Link
-              href='/checkout'
-              className='btn-primary flex w-full sm:w-1/2 justify-center '
-            >
-              Next
-            </Link>
-          </div>
-        </div>
+        <AddressForm />
       </div>
     </div>
   )
